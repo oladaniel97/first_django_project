7f285d45b4cbf94f3d579c446dc622c3272042c6
daniel amodu <danielolayinka97@gmail.com>
2022-10-25 21:12:57 +0100

    new additions

--- songcrud/musicapp/models.py
+++ songcrud/musicapp/models.py
@@ -1,3 +1,32 @@
+from locale import CHAR_MAX
 from django.db import models
+from datetime import datetime
+from django.template.defaultfilters import truncatechars
 
 # Create your models here.
+
+class Artiste(models.Model):
+    first_name = models.CharField(max_length = 40)
+    last_name = models.CharField(max_length = 40)
+    age = models.IntegerField()
+
+    def __str__(self) :
+        return self.first_name + " " + self.last_name
+
+class Song(models.Model):
+    artiste_id = models.ForeignKey(Artiste, on_delete=models.CASCADE)
+    title = models.CharField(max_length = 400)
+    date_released = models.DateField(default=datetime.today)
+    likes = models.IntegerField()
+
+    def __str__(self) :
+        return self.title
+
+class Lyric(models.Model):
+    song_id = models.OneToOneField(Song, on_delete=models.CASCADE)
+    content = models.CharField(max_length = 4000)
+
+    def __str__(self):
+        return truncatechars(self.content, 50)
+    #def __str__(self) :
+        #return self.content
